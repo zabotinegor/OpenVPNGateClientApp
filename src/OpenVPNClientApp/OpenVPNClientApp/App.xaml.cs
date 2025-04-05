65f2399a83417f9239d3d3e76a9858973f45b77f
Yahor Zabotsin <zabotinegor@mail.ru>
2025-04-05 06:44:22 +0200

    app basis

---
+++ src/OpenVPNClientApp/OpenVPNClientApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenVPNClientApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
