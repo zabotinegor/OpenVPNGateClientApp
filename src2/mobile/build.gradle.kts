d323d94971d11d57b3fdb7f356eb60bda6722e87
Yahor Zabotsin <zabotinegor@mail.ru>
2025-10-14 12:59:11 +0200

    draft mobile version

--- src2/mobile/build.gradle.kts
+++ src2/mobile/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":core"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
