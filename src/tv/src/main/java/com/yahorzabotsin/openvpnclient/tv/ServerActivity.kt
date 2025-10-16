64859f62f715b4c0b2e53b2d12b78914d87e74d3
Yahor Zabotsin <zabotinegor@mail.ru>
2025-10-16 13:38:53 +0200

    draft country filtering

--- src/tv/src/main/java/com/yahorzabotsin/openvpnclient/tv/ServerActivity.kt
+++ src/tv/src/main/java/com/yahorzabotsin/openvpnclient/tv/ServerActivity.kt
@@ -4,13 +4,17 @@ import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.ProgressBar
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 import com.yahorzabotsin.openvpnclient.core.R as coreR
+import com.yahorzabotsin.openvpnclient.core.servers.Server
 import com.yahorzabotsin.openvpnclient.core.servers.ServerRepository
 import com.yahorzabotsin.openvpnclient.core.ui.MarginItemDecoration
 import com.yahorzabotsin.openvpnclient.core.ui.ServerAdapter
@@ -18,6 +22,7 @@ import kotlinx.coroutines.launch
 
 class ServerActivity : AppCompatActivity() {
     private val serverRepository = ServerRepository()
+    private lateinit var servers: List<Server>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +40,7 @@ class ServerActivity : AppCompatActivity() {
 
         val recyclerView = findViewById<RecyclerView>(R.id.servers_recycler_view)
         val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
+        val countrySpinner = findViewById<Spinner>(R.id.country_spinner)
 
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.addItemDecoration(MarginItemDecoration(resources.getDimensionPixelSize(coreR.dimen.server_item_margin)))
@@ -42,11 +48,35 @@ class ServerActivity : AppCompatActivity() {
         lifecycleScope.launch {
             progressBar.visibility = View.VISIBLE
             recyclerView.visibility = View.GONE
+            countrySpinner.visibility = View.GONE
             try {
-                val servers = serverRepository.getServers()
+                servers = serverRepository.getServers()
+                val countries = mutableListOf("All")
+                countries.addAll(servers.map { it.country.name }.distinct())
+                val adapter = ArrayAdapter(this@ServerActivity, android.R.layout.simple_spinner_item, countries)
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                countrySpinner.adapter = adapter
+
+                countrySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                        val selectedCountry = parent.getItemAtPosition(position).toString()
+                        if (selectedCountry == "All") {
+                            recyclerView.adapter = ServerAdapter(servers)
+                        } else {
+                            val filteredServers = servers.filter { it.country.name == selectedCountry }
+                            recyclerView.adapter = ServerAdapter(filteredServers)
+                        }
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>) {
+                        // Do nothing
+                    }
+                }
+
                 recyclerView.adapter = ServerAdapter(servers)
                 progressBar.visibility = View.GONE
                 recyclerView.visibility = View.VISIBLE
+                countrySpinner.visibility = View.VISIBLE
             } catch (e: Exception) {
                 Log.e("ServerActivity", "Error getting servers", e)
                 progressBar.visibility = View.GONE
