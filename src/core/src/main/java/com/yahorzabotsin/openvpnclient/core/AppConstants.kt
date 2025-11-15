4dcb0f65046eb35cb27728be9106c32bf12b4222
Yahor Zabotsin <zabotinegor@mail.ru>
2025-11-15 22:13:20 +0100

    added new url

--- src/core/src/main/java/com/yahorzabotsin/openvpnclient/core/AppConstants.kt
+++ src/core/src/main/java/com/yahorzabotsin/openvpnclient/core/AppConstants.kt
@@ -1,6 +1,9 @@
 package com.yahorzabotsin.openvpnclient.core
 
 object ApiConstants {
-    const val BASE_URL = "https://www.vpngate.net/"
-    const val API_ENDPOINT = "/api/iphone/"
+    const val PRIMARY_BASE_URL = "https://openvpngateserver.azurewebsites.net/"
+    const val PRIMARY_API_ENDPOINT = "api/v1/servers/active"
+
+    const val FALLBACK_BASE_URL = "https://www.vpngate.net/"
+    const val FALLBACK_API_ENDPOINT = "api/iphone/"
 }
--- src/core/src/main/java/com/yahorzabotsin/openvpnclient/core/servers/ServerRepository.kt
+++ src/core/src/main/java/com/yahorzabotsin/openvpnclient/core/servers/ServerRepository.kt
@@ -1,5 +1,6 @@
 package com.yahorzabotsin.openvpnclient.core.servers
 
+import android.util.Log
 import com.yahorzabotsin.openvpnclient.core.ApiConstants
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -7,28 +8,51 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import java.util.concurrent.TimeUnit
 
-interface VpnGateApi {
-    @GET(ApiConstants.API_ENDPOINT)
+interface PrimaryVpnApi {
+    @GET(ApiConstants.PRIMARY_API_ENDPOINT)
+    suspend fun getServers(): String
+}
+
+interface FallbackVpnGateApi {
+    @GET(ApiConstants.FALLBACK_API_ENDPOINT)
     suspend fun getServers(): String
 }
 
 class ServerRepository {
+
+    private companion object {
+        private const val TAG = "ServerRepository"
+    }
 
     private val okHttpClient = OkHttpClient.Builder()
         .readTimeout(60, TimeUnit.SECONDS)
         .connectTimeout(60, TimeUnit.SECONDS)
         .build()
 
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(ApiConstants.BASE_URL)
+    private val primaryRetrofit = Retrofit.Builder()
+        .baseUrl(ApiConstants.PRIMARY_BASE_URL)
         .client(okHttpClient)
         .addConverterFactory(ScalarsConverterFactory.create())
         .build()
 
-    private val vpnGateApi = retrofit.create(VpnGateApi::class.java)
+    private val fallbackRetrofit = Retrofit.Builder()
+        .baseUrl(ApiConstants.FALLBACK_BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .build()
+
+    private val primaryApi = primaryRetrofit.create(PrimaryVpnApi::class.java)
+    private val fallbackApi = fallbackRetrofit.create(FallbackVpnGateApi::class.java)
 
     suspend fun getServers(): List<Server> {
-        val response = vpnGateApi.getServers()
+        val response = try {
+            Log.d(TAG, "Requesting servers from PRIMARY: ${ApiConstants.PRIMARY_BASE_URL}${ApiConstants.PRIMARY_API_ENDPOINT}")
+            primaryApi.getServers()
+        } catch (e: Exception) {
+            Log.w(TAG, "Primary servers endpoint failed, falling back to VPNGate: ${ApiConstants.FALLBACK_BASE_URL}${ApiConstants.FALLBACK_API_ENDPOINT}", e)
+            fallbackApi.getServers()
+        }
+
         return response.lines().drop(2).filter { it.isNotBlank() }.mapNotNull { line ->
             val values = line.split(",")
             if (values.size < 15) {
