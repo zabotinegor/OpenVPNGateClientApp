8f46226b83a1a02c9dccc440024297a08f453532
Yahor Zabotsin <56268079+zabotinegor@users.noreply.github.com>
2026-02-07 22:42:41 +0100

    Attemt to move "About Page" to MVVM (#32)
    * moved about page to mvvm
    * tests fix
    * Fix year provider for pre-26 devices

--- src/core/src/main/java/com/yahorzabotsin/openvpnclientgate/core/di/CoreDi.kt
+++ src/core/src/main/java/com/yahorzabotsin/openvpnclientgate/core/di/CoreDi.kt
@@ -1,9 +1,21 @@
 package com.yahorzabotsin.openvpnclientgate.core.di
 
+import com.yahorzabotsin.openvpnclientgate.core.about.AboutInfoProvider
+import com.yahorzabotsin.openvpnclientgate.core.about.AboutLinksProvider
+import com.yahorzabotsin.openvpnclientgate.core.about.DefaultAboutInfoProvider
+import com.yahorzabotsin.openvpnclientgate.core.about.DefaultAboutLinksProvider
+import com.yahorzabotsin.openvpnclientgate.core.about.ElapsedRealtimeProvider
+import com.yahorzabotsin.openvpnclientgate.core.about.LogExportInteractor
+import com.yahorzabotsin.openvpnclientgate.core.about.LogExportUseCase
+import com.yahorzabotsin.openvpnclientgate.core.about.SystemElapsedRealtimeProvider
+import com.yahorzabotsin.openvpnclientgate.core.about.SystemYearProvider
+import com.yahorzabotsin.openvpnclientgate.core.about.YearProvider
 import com.yahorzabotsin.openvpnclientgate.core.servers.ServerRepository
 import com.yahorzabotsin.openvpnclientgate.core.servers.VpnServersApi
 import com.yahorzabotsin.openvpnclientgate.core.settings.UserSettingsStore
+import com.yahorzabotsin.openvpnclientgate.core.ui.about.AboutViewModel
 import okhttp3.OkHttpClient
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
@@ -32,4 +44,12 @@ val coreModule = module {
     single<UserSettingsStore> { UserSettingsStore }
 
     single { ServerRepository(get(), get()) }
+
+    single<YearProvider> { SystemYearProvider() }
+    single<AboutInfoProvider> { DefaultAboutInfoProvider(get(), get()) }
+    single<AboutLinksProvider> { DefaultAboutLinksProvider() }
+    single<ElapsedRealtimeProvider> { SystemElapsedRealtimeProvider() }
+    single<LogExportInteractor> { LogExportUseCase(get()) }
+
+    viewModel { AboutViewModel(get(), get(), get(), get()) }
 }
--- src/core/src/test/java/com/yahorzabotsin/openvpnclientgate/core/di/CoreDiTest.kt
+++ src/core/src/test/java/com/yahorzabotsin/openvpnclientgate/core/di/CoreDiTest.kt
@@ -2,6 +2,7 @@ package com.yahorzabotsin.openvpnclientgate.core.di
 
 import com.yahorzabotsin.openvpnclientgate.core.servers.ServerRepository
 import com.yahorzabotsin.openvpnclientgate.core.servers.VpnServersApi
+import com.yahorzabotsin.openvpnclientgate.core.settings.UserSettingsStore
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.ResponseBody
 import okhttp3.ResponseBody.Companion.toResponseBody
@@ -24,13 +25,13 @@ class CoreDiTest {
     }
 
     @Test
-    fun `api can be overridden`() {
+    fun `api can be injected from custom module`() {
         val fakeApi = FakeVpnServersApi()
         val koin = koinApplication {
             modules(
-                coreModule,
-                module(override = true) {
+                module {
                     single<VpnServersApi> { fakeApi }
+                    single { ServerRepository(get(), UserSettingsStore) }
                 }
             )
         }.koin
