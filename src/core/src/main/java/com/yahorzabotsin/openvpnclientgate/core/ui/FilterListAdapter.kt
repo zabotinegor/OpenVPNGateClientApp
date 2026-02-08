73a6bd4545067f49cec242a0fa0418625b7f23e7
Yahor Zabotsin <zabotinegor@mail.ru>
2026-02-08 19:48:49 +0100

    tests

--- src/core/src/main/java/com/yahorzabotsin/openvpnclientgate/core/ui/FilterListAdapter.kt
+++ src/core/src/main/java/com/yahorzabotsin/openvpnclientgate/core/ui/FilterListAdapter.kt
@@ -16,6 +16,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
