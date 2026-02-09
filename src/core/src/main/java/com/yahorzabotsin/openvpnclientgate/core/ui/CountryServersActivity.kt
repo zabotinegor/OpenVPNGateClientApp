9dbfb53f1829b36f322a5ca2b3bcdc92abc903e9
Yahor Zabotsin <zabotinegor@mail.ru>
2026-02-09 10:57:48 +0100

    build fix

--- src/core/src/main/java/com/yahorzabotsin/openvpnclientgate/core/ui/CountryServersActivity.kt
+++ src/core/src/main/java/com/yahorzabotsin/openvpnclientgate/core/ui/CountryServersActivity.kt
@@ -3,6 +3,7 @@ package com.yahorzabotsin.openvpnclientgate.core.ui
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
