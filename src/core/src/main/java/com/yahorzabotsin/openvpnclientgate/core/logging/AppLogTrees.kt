810feffcdb0da14caeb70677cddf9ec631a55e73
Yahor Zabotsin <zabotinegor@mail.ru>
2026-02-10 22:04:17 +0100

    Log throwable atomically in Timber trees

--- src/core/src/main/java/com/yahorzabotsin/openvpnclientgate/core/logging/AppLogTrees.kt
+++ src/core/src/main/java/com/yahorzabotsin/openvpnclientgate/core/logging/AppLogTrees.kt
@@ -6,10 +6,8 @@ import timber.log.Timber
 class AppDebugTree : Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
         val resolvedTag = tag ?: LogTags.APP
-        Log.println(priority, resolvedTag, message)
-        if (t != null) {
-            Log.println(priority, resolvedTag, Log.getStackTraceString(t))
-        }
+        val fullMessage = if (t == null) message else "$message\n${Log.getStackTraceString(t)}"
+        Log.println(priority, resolvedTag, fullMessage)
     }
 }
 
@@ -17,10 +15,8 @@ class AppReleaseTree : Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
         if (priority == Log.DEBUG || priority == Log.VERBOSE) return
         val resolvedTag = tag ?: LogTags.APP
-        Log.println(priority, resolvedTag, message)
-        if (t != null) {
-            Log.println(priority, resolvedTag, Log.getStackTraceString(t))
-        }
+        val fullMessage = if (t == null) message else "$message\n${Log.getStackTraceString(t)}"
+        Log.println(priority, resolvedTag, fullMessage)
     }
 }
 
